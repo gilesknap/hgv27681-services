ebef7a4159324ee3858d2fc55f009e84e6e192dd
giles knap <gilesknap@gmail.com>
2025-07-24 15:39:41 +0100

    fix addrlist for ca-gateway

--- services/bl01t-gateways/config/environment.sh
+++ services/bl01t-gateways/config/environment.sh
@@ -3,15 +3,12 @@
 if [[ -z ${EPICS_CA_ADDR_LIST} ]]; then
   echo "EPICS_CA_ADDR_LIST is not set, discovering IOCs DNS names in the namespace"
   export IPS="$(python3 /config/get_ioc_list.py --dns-names)"
-else
-if [[ -n ${EPICS_CA_ADDR_LIST} ]]; then
-  cip="-cip"
 fi
 
 if [[ -z ${EPICS_PVA_ADDR_LIST} ]]; then
   echo "EPICS_PVA_ADDR_LIST is not set, discovering IOCs DNS names in the namespace"
   export IPS="$(python3 /config/get_ioc_list.py --dns-names)"
-else
-if [[ -n ${EPICS_PVA_ADDR_LIST} ]]; then
-  cip="-cip"
 fi
+
+echo "Using EPICS_CA_ADDR_LIST: ${EPICS_CA_ADDR_LIST}"
+echo "Using EPICS_PVA_ADDR_LIST: ${EPICS_PVA_ADDR_LIST}"
--- services/bl01t-gateways/config/start-ca.sh
+++ services/bl01t-gateways/config/start-ca.sh
@@ -4,8 +4,12 @@ THIS_DIR="$(dirname "$(readlink -f "$0")")"
 
 source "${THIS_DIR}/environment.sh"
 
+if [[ -n ${EPICS_CA_ADDR_LIST} ]]; then
+  cip="-cip"
+  addrlist=\"${EPICS_CA_ADDR_LIST}\"
+fi
 # start the CA Gateway
 set -x
-/epics/ca-gateway/bin/linux-x86_64/gateway -sport ${CA_SERVER_PORT} ${cip} "${EPICS_CA_ADDR_LIST}"\
+/epics/ca-gateway/bin/linux-x86_64/gateway -sport ${CA_SERVER_PORT} ${cip} ${addrlist} \
    -pvlist /config/pvlist -access /config/access \
    -log /dev/stdout -debug ${CA_DEBUG:-0}
