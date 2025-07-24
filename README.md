2aca209bd5e894e26bbd7e66b6fcbcb0a5fcaf83
giles knap <gilesknap@gmail.com>
2025-07-24 08:38:28 +0100

    fix IPs and forwarding script

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # hgv27681 IOC Instances and Services
 
 This repository holds the a definition of hgv27681 IOC Instances and services. Each sub folder of the `services` directory contains a helm chart for a specific service or IOC.
+
+Here we have implemented INFN's fixed clusterIP helm function `allocateIpFromName`.
+
+- IP addresses in argus allocated by allocateIpFromName
+  - CIDR = baseIp: "10.96.0.0/12"
+  - hgv27681.bl01t-di-cam-01 = 10.111.7.5
+  - hgv27681.bl01t-mo-sim-01 = 10.103.7.44
--- ca-forward.sh
+++ ca-forward.sh
@@ -4,14 +4,21 @@ set -e
 # restart the gateways service to pick up any new/restarted IOCs
 # Note: ArgoCD will automatically start a new pod and deletion will complete
 # when the new pod is ready.
-podname=$(kubectl get pods -l app=bl01t-gateways -o name)
-kubectl delete $podname --wait=true
 
-# make sure the new pod is ready
-kubectl wait deploy/bl01t-gateways --for=jsonpath='{.status.readyReplicas}=1'
-sleep 2
+if [[ -n $RESTART ]]; then
+    echo "Restarting gateways service ..."
+    podname=$(kubectl get pods -l app=bl01t-gateways -o name)
+    kubectl delete $podname --wait=true
+
+    # make sure the new pod is ready
+    kubectl wait deploy/bl01t-gateways --for=jsonpath='{.status.readyReplicas}=1'
+    sleep 2
+fi
 
 # forward ports for CA and PVA to the gateways service
-kubectl port-forward service/bl01t-gateways 9064 9065
+# kubectl port-forward service/bl01t-gateways 9064 9065
 
+# UPDATE - just forward direct to the gateways service
+# this works over VPN and when the science/corp firewall is blocking the ports
+ssh -f -L 9064:172.23.169.1:9064 -L 9065:172.23.169.1:9065 hgv27681@pc0116 -N
 
