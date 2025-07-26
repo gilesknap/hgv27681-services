f2e1afab31c02115f95c8790a705f3cabb4fd693
giles knap <gilesknap@gmail.com>
2025-07-26 07:02:33 +0100

    remove auto_addr in phoebus settings

--- ca-test.sh
+++ ca-test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # set up the environment for caget for this shell
-export EPICS_CA_NAME_SERVERS=localhost
-export EPICS_CA_SERVER_PORT=9064
+export EPICS_CA_NAME_SERVERS=localhost:9064
+export EPICS_PVA_NAME_SERVERS=localhost:9075
+export EPICS_CA_MAX_ARRAY_BYTES=6000000
+export EPICS_PVA_MAX_ARRAY_BYTES=6000000
+export EPICS_CA_AUTO_ADDR_LIST=NO
+export EPICS_PVA_AUTO_ADDR_LIST=NO
 
 # demonstrate caget works
 caget BL01T-DI-CAM-01:UPTIME
+# demonstrate pvxget works
+pvxget BL01T-DI-CAM-01:PVA:OUTPUT | grep uncompressedSize
